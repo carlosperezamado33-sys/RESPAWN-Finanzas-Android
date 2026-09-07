3e9c1e9165f0b857069bacc7c2a9c77c0607bb13
carlosperezamado33-sys <carlosperezamado33@gmail.com>
2026-09-07 15:49:55 +0200

    Update MainScreens.kt

--- app/src/main/java/com/respawn/finanzas/ui/MainScreens.kt
+++ app/src/main/java/com/respawn/finanzas/ui/MainScreens.kt
@@ -2,6 +2,7 @@ package com.respawn.finanzas.ui
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
