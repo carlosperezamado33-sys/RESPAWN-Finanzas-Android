936abeaffdd9b9e25f1b58cb121717200196a553
carlosperezamado33-sys <carlosperezamado33@gmail.com>
2026-09-09 22:10:33 +0200

    v0.10: subir versión a 0.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.respawn.finanzas"
         minSdk = 26
         targetSdk = 37
-        versionCode = 10
-        versionName = "0.9.1"
+        versionCode = 11
+        versionName = "0.10.0"
     }
 
     buildFeatures {
